533ee448e1713d952ef27656543e5f566c08e482
April12895 <dwiapriliahaliza08@gmail.com>
2021-06-15 15:09:32 +0700

    MINGGU 12

---
+++ MINGGU 12/april_belajarlaravel/resources/views/gambar_april.blade.php
@@ -0,0 +1,5 @@
+<img src= "{{ asset('img/gambar1.jpg') }}" alt="">
+<img src= "{{ asset('img/gambar2.jpg') }}" alt="">
+<img src= "{{ asset('img/gambar3.jpg') }}" alt="">
+<img src= "{{ asset('img/gambar4.jpeg') }}" alt="">
+<img src= "{{ asset('img/gambar5.jpeg') }}" alt="">
